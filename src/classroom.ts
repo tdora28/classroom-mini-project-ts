86c90388bc8221feb77eac778aacd0c4e43dced7
tdora28 <tokaidora88@gmail.com>
2024-02-22 14:58:23 +0200

    Implement getNumberOfGrades

--- src/classroom.ts
+++ src/classroom.ts
@@ -1,24 +1,14 @@
-export function getNumberOfGrades(grades) {
-    
+// `getNumberOfGrades` should return the number of grades
+export function getNumberOfGrades(grades: number[]): number {
+  return grades.length;
 }
 
-export function getSumGrades(grades) {
-    
-}
+export function getSumGrades(grades) {}
 
-export function getAverageValue(grades) {
-    
-}
+export function getAverageValue(grades) {}
 
-export function getPassingGrades(grades) {
-   
-}
+export function getPassingGrades(grades) {}
 
+export function getFailingGrades(grades) {}
 
-export function getFailingGrades(grades) {
-    
-}
-
-export function getRaisedGrades(grades) {
-    
-}
+export function getRaisedGrades(grades) {}

d3dabfe029d58aaeb6fb656384a75115af3069d3
tdora28 <tokaidora88@gmail.com>
2024-02-22 15:01:48 +0200

    Implement getSumGrades

--- src/classroom.ts
+++ src/classroom.ts
@@ -3,7 +3,10 @@ export function getNumberOfGrades(grades: number[]): number {
   return grades.length;
 }
 
-export function getSumGrades(grades) {}
+// `getSumGrades` should return the sum of all the grades
+export function getSumGrades(grades: number[]): number {
+  return grades.reduce((acc, curr) => acc + curr, 0);
+}
 
 export function getAverageValue(grades) {}
 

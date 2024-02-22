3a2cbb4bc3e9215821300a72942c193500471de9
tdora28 <tokaidora88@gmail.com>
2024-02-22 14:54:10 +0200

    Add starter files

---
+++ src/classroom.ts
@@ -0,0 +1,24 @@
+export function getNumberOfGrades(grades) {
+    
+}
+
+export function getSumGrades(grades) {
+    
+}
+
+export function getAverageValue(grades) {
+    
+}
+
+export function getPassingGrades(grades) {
+   
+}
+
+
+export function getFailingGrades(grades) {
+    
+}
+
+export function getRaisedGrades(grades) {
+    
+}
